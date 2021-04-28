328881d9b1df296674ff49be4284d7f91ebd04d2
Zachary <zac.hong01@gmail.com>
2021-04-28 22:38:21 +0800

    Create auction.sol

---
+++ auction.sol
@@ -0,0 +1,47 @@
+pragma solidity ^0.6.0;
+
+contract Auction {
+    string public name = "Attention Auction";
+    address public owner;
+
+    struct display {
+        uint256 price;
+        string url;
+        string name;
+        address contributor;
+    }
+
+    display public currentDisplay;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function newDisplay(
+        uint256 _price,
+        string memory _url,
+        string memory _name
+    ) public payable {
+        require(currentDisplay.price < _price);
+        currentDisplay.price = _price;
+        currentDisplay.url = _url;
+        currentDisplay.name = _name;
+    }
+
+    function getDisplay()
+        public
+        view
+        returns (
+            uint256 _price,
+            string memory _url,
+            string memory _name
+        )
+    {
+        return (currentDisplay.price, currentDisplay.url, currentDisplay.name);
+    }
+
+    function withdraw() public {
+        address payable _owner = payable(owner);
+        _owner.transfer(address(this).balance);
+    }
+}
